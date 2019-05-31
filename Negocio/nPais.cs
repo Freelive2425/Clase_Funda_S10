e1c1c65bad4fd4f58bdfb1d9711a1aef1b27f6ae
alumnos <alumnos@VA107-20>
2019-05-31 18:23:11 -0500

    Add project files.

---
+++ Negocio/nPais.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+using Datos;
+
+namespace Negocio
+{
+    public class nPais
+    {
+        dPais paisd;
+
+
+        public nPais()
+        {
+            paisd = new dPais();
+        }
+        public string RegistrarPais(string nombre)
+        {
+            ePais pais = new ePais()
+            {
+                Nombrepais = nombre
+            };
+            return paisd.Insertar(pais);
+        }
+        public string Modificarpais(int idpais, string nombrepais)
+        {
+            ePais pais = new ePais()
+            {
+                Idpais = idpais,
+                Nombrepais = nombrepais
+            };
+            return paisd.Modificar(pais);
+        }
+        public string Eliminarpais(int id)
+        {
+            return paisd.Eliminar(id);
+        }
+        public List<ePais> Listarpais()
+        {
+            return paisd.ListarTodo();
+        }
+
+
+    }
+}
